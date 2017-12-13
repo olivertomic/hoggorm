a902b80b0f6bf201de626c7b3e2eddde6b23e3f8
Thomas Graff <graff.thomas@gmail.com>
2017-12-13 13:24:35 +0100

    Enable parsing of Google style docstrings
    See: http://www.sphinx-doc.org/en/stable/ext/napoleon.html

--- docs/conf.py
+++ docs/conf.py
@@ -30,7 +30,7 @@ needs_sphinx = '1.5'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -75,6 +75,21 @@ pygments_style = 'sphinx'
 
 # If true, `todo` and `todoList` produce output, else they produce nothing.
 todo_include_todos = False
+
+
+# -- Options for Napoleon pre-processor -----------------------------------
+
+napoleon_google_docstring = True
+napoleon_numpy_docstring = False
+# napoleon_include_init_with_doc = False
+# napoleon_include_private_with_doc = False
+# napoleon_include_special_with_doc = True
+# napoleon_use_admonition_for_examples = False
+# napoleon_use_admonition_for_notes = False
+# napoleon_use_admonition_for_references = False
+# napoleon_use_ivar = False
+# napoleon_use_param = True
+# napoleon_use_rtype = True
 
 
 # -- Options for HTML output ----------------------------------------------
