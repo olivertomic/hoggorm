e313fa08e6a1397bb978d2b378ff3b3e17a9f935
Thomas Graff <graff.thomas@gmail.com>
2017-12-22 13:45:59 +0100

    Added testing of nipalsPLS1

--- tests/test_plsr.py
+++ tests/test_plsr.py
@@ -13,7 +13,8 @@ import numpy as np
 
 import pytest
 
-from hoggorm import nipalsPLS2 as PLSR
+from hoggorm import nipalsPLS1 as PLSR1
+from hoggorm import nipalsPLS2 as PLSR2
 
 
 # If the following equation is element-wise True, then allclose returns True.
@@ -28,16 +29,38 @@ ATTRS = [
 ]
 
 
-def test_api_verify(ldat, sdat):
+def test_api2_verify(ldat, sdat):
     print(ldat.shape, sdat.shape)
-    plsr1 = PLSR(arrX=ldat, arrY=sdat, numComp=3, Xstand=False, Ystand=False, cvType=["loo"])
+    plsr1 = PLSR2(arrX=ldat, arrY=sdat, numComp=3, Xstand=False, Ystand=False, cvType=["loo"])
     print('plsr1', plsr1)
-    plsr2 = PLSR(ldat, sdat)
+    plsr2 = PLSR2(ldat, sdat)
     print('plsr2', plsr2)
-    plsr3 = PLSR(ldat, sdat, numComp=300, cvType=["loo"])
+    plsr3 = PLSR2(ldat, sdat, numComp=300, cvType=["loo"])
     print('plsr3', plsr3)
-    plsr4 = PLSR(arrX=ldat, arrY=sdat, cvType=["loo"], numComp=5, Xstand=False, Ystand=False)
+    plsr4 = PLSR2(arrX=ldat, arrY=sdat, cvType=["loo"], numComp=5, Xstand=False, Ystand=False)
     print('plsr4', plsr4)
-    plsr5 = PLSR(ldat, sdat, Xstand=True, Ystand=True)
+    plsr5 = PLSR2(ldat, sdat, Xstand=True, Ystand=True)
+    print('plsr5', plsr5)
+    assert True
+
+
+
+
+
+def test_api1_verify(ldat, sdat):
+    svec = np.column_stack(sdat[:,0]).T
+    print('\n')
+    print('Shapes:', ldat.shape, svec.shape)
+    print(ldat)
+    print(svec)
+    plsr1 = PLSR1(arrX=ldat, vecy=svec, numComp=3, Xstand=False, Ystand=False, cvType=["loo"])
+    print('plsr1', plsr1)
+    plsr2 = PLSR1(ldat, svec)
+    print('plsr2', plsr2)
+    plsr3 = PLSR1(ldat, svec, numComp=300, cvType=["loo"])
+    print('plsr3', plsr3)
+    plsr4 = PLSR1(arrX=ldat, vecy=svec, cvType=["loo"], numComp=5, Xstand=False, Ystand=False)
+    print('plsr4', plsr4)
+    plsr5 = PLSR1(ldat, svec, Xstand=True, Ystand=True)
     print('plsr5', plsr5)
     assert True
