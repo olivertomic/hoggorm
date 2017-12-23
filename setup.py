85a04a678e4dea8a2602746d4e78249e564386ae
Thomas Graff <graff.thomas@gmail.com>
2017-12-23 08:30:46 +0100

    Update release recipe

--- setup.py
+++ setup.py
@@ -4,17 +4,24 @@ See:
 https://packaging.python.org/en/latest/distributing.html
 https://github.com/pypa/sampleproject
 
-How to generate new PyPI package
+How to make a new release:
+
 Update version information
+> someeditor hoggorm/version.py
+
 Run tests
+> pytest
 
 Generate package files
-python setup.py sdist
-python setup.py bdist_wheel
+> python setup.py sdist
+> python setup.py bdist_wheel
 
-Uplad package files
-twine upload dist/*
+Upload package to PyPI
+> twine upload dist/*
 
+Tag the new release
+> git tag -a vX.Y.Z -m "Tag release X.Y.Z"
+> git push origin --tags
 """
 from setuptools import setup, find_packages
 
