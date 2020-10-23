195a0e486da69b70669ab4f2e1b4aa9a38aad887
Andreas Fehlner <fehlner@arcor.de>
2020-10-23 21:35:06 +0200

    #DC add codacy

--- setup.py
+++ setup.py
@@ -1,5 +1,26 @@
 #!/usr/bin/env python
-"""The setup script."""
+"""The setup script.
+
+How to make a new release:
+
+Update version information
+> someeditor hoggorm/version.py
+
+Run tests
+> pytest
+
+Generate package files
+> python setup.py sdist
+> python setup.py bdist_wheel
+
+Upload package to PyPI
+> twine upload dist/*
+
+Tag the new release
+> git tag -a vX.Y.Z -m "Tag release X.Y.Z"
+> git push origin --tags
+"""
+
 
 from setuptools import setup
 
